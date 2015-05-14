ae32d29ebc9758a5987f29d08a90b1dc4d52c053
Marco Minetti <marco.minetti@novetica.org>
2015-05-14 13:01:30 +0200

    Added FuncDocumentation extension.

---
+++ src/extensions/default/FuncDocumentation/README.md
@@ -0,0 +1,78 @@
+# FuncDocr
+
+A brackets extension to generate JS/PHPDocs for your functions.
+
+## How to Use
+* Open a JS or PHP file
+* set your cursor on a function declaration
+	* `function cool(stuff)`
+* Use the ShortCut `Ctrl-Alt-D` (Win) or `Ctrl-Shift-D` (Mac) to start the documentation or `/**` + Enter
+
+
+The extension will provide a function documentation:
+
+Natural way: `/**` + Enter
+
+![](https://cloud.githubusercontent.com/assets/4931746/4238565/c820925e-39d9-11e4-998b-693c25b3a5b1.gif)
+
+With shortcut:
+
+![](https://cloud.githubusercontent.com/assets/4931746/4141317/f9303404-33ac-11e4-8e3c-c72925b4e550.gif)
+
+
+
+`[[Description]]` will be selected so you can start to type. To jump to the next `[[tag]]` you can use `Tab` or jump to the last with `Shift-Tab`. The indenting updates on a tab jump (live indenting).
+
+You will get hints for the `[[Type]]` tag.
+
+![Type hints](https://cloud.githubusercontent.com/assets/4931746/3998983/b3eba9ba-294c-11e4-988b-4330735635fd.png)
+
+Have fun and stay tuned!
+
+## You're rich?
+You don't need a second yacht and have some coins to spend? Here I am :)
+
+[![Flattr this git repo](http://api.flattr.com/button/flattr-badge-large.png)](https://flattr.com/submit/auto?user_id=Wikunia&url=https://github.com/Wikunia/brackets-FuncDocr&title=Brackets-FuncDocr&language=javascript&tags=github&category=software)
+
+## v0.8.4
+
+- It's possible to disable the autoindent functionality in the settings menu.
+- `@callback` links will be visible as `[[Type]]` hints.
+
+## v0.8.0
+You can use several JSDoc options to describe your function
+and you can dblclick on `[[...]]` to edit the tags.
+
+![funcdocr0 8 0](https://cloud.githubusercontent.com/assets/4931746/5670323/2c266c44-977f-11e4-8683-6b1923c66edf.gif)
+
+
+## v0.7.0
+Document your react functions!
+![funcdocr0 7 0](https://cloud.githubusercontent.com/assets/4931746/5670498/a3024bf2-9780-11e4-8bd9-448ef4f04496.gif)
+
+
+## v0.6.0
+## @link tags
+
+![linkTag](https://cloud.githubusercontent.com/assets/4931746/5079729/78a7cc3e-6eb7-11e4-8502-7718bf7c0c97.gif)
+
+Easy way to add @link tags to your docs!
+
+## v0.5.3
+### Default values!
+
+![Default values](https://cloud.githubusercontent.com/assets/4931746/4649697/18f8859c-548d-11e4-9f86-9a9a669b71f2.gif)
+
+## v0.5.0
++ It's possible to generate the doc block with `/**` + Enter. You only need to type `/**` in the line above the function declaration and use enter to start the documentation.
+
+## v0.4.0
++ The padding for correct indentation updates on a tab jump
+
+## v0.3.0
++ It's possible to update a doc block if you add a parameter or delete one (you need to use the shortcut again)
+
+## v0.2.0
++ You get the tag `@returns` if the function returns a value
++ The `[[Type]]` is recognized if possible.
+
---
+++ src/extensions/default/FuncDocumentation/hints.js
@@ -0,0 +1,335 @@
+// =========================================================================
+// Hint Manager
+// =========================================================================
+define(['text!definitions/default.json',
+        'text!definitions/js.json',
+        'text!definitions/php.json'],
+    function (defaultDef, javascriptDef, phpDef) {
+        var DocumentManager     = brackets.getModule('document/DocumentManager');
+		var EditorManager       = brackets.getModule('editor/EditorManager');
+		var PreferencesManager	= brackets.getModule('preferences/PreferencesManager');
+
+		/**
+         * Parse Documentation Definitions, used to populate tag suggestions
+         */
+        var DOC_DEFINITIONS = {
+            default: JSON.parse(defaultDef),
+            javascript: JSON.parse(javascriptDef),
+            php: JSON.parse(phpDef)
+        };
+        DOC_DEFINITIONS.jsx			 = DOC_DEFINITIONS.javascript;
+        DOC_DEFINITIONS.coffeescript = DOC_DEFINITIONS.javascript;
+        DOC_DEFINITIONS.livescript 	 = DOC_DEFINITIONS.javascript;
+    
+        var DOCBLOCK_STATIC_HINTS = /(\[\[[^\]]+\<[^\]]+\>\]\])/;
+        var DOCBLOCK_FIELD = /(\[\[[^\]]+\]\])/;
+        var CALLBACK = /\/\*\*[\s\S]*?@callback\s(\S*?)\s[\s\S]*?\*\//g;
+        var FUNC_DEFINITION_ROW = /^[\S\s]*?\*\/([\S\s]*?{)/g;
+		var FUNC_DEFINITION	    = /^(var (.*)=\s*(?:function(.*)|React.createClass\s*\((?:.*))|function (.*?)|(.*?):\s*?function(.*?)|([^.]*?)\.(prototype\.)?([^.]*?)\s*?=\s*?function(.*?)|([A-Za-z\$\_][A-Za-z\$\_0-9]*)?\s*\(([^\)]*)\)\s*{)/;
+		var REGEX_END			= /(\n|\r|$)/;
+
+
+        /**
+         * DocrHint constructor
+         * @param {Object} importFuncs functions that will be used in this class and must be imported
+         */
+        function DocrHint(importFuncs) {
+
+            var docrHintThis = this;
+            $.each(importFuncs, function (key, func) {
+                docrHintThis[key] = func;
+            });
+
+            docrHintThis.docDefinitions = DOC_DEFINITIONS;
+        }
+
+        /**
+         * Checks, if it is possible to give hints inside the current docblock.
+         * @param   {editor}  editor       current brackets editor
+         * @param   {String}  implicitChar implicit character
+         * @returns {Boolean} true for has hints otherwise false
+         */
+        DocrHint.prototype.hasHints = function (editor, implicitChar) {
+            this.editor = editor;
+            this.selection = editor.getSelectedText();
+            this.pos = editor.getCursorPos();
+            this.insideDocPos = this.insideDocBlock(this.pos);
+            if (this.insideDocPos) {
+                this.pos = editor.getCursorPos();
+				if (this.selection.substr(0,2) == '[[' && this.selection.substr(-2,2) == ']]') {
+					this.implicitChar = this.selection;
+					return true;
+				} else {
+                    if (implicitChar == '@') {
+                        this.removeSelection = true;
+                        this.implicitChar = "@";
+                        return true;
+                    } else if (DOCBLOCK_STATIC_HINTS.exec(this.selection)) {
+                        this.removeSelection = true;
+                        this.implicitChar = this.selection;
+                        return true;
+                    }
+                }
+            }
+            return false;
+        };
+
+        /**
+         * @cal
+         * Get the hints for a selection
+         * uses {@link removeWrongHints	removeWrongHints}
+         * @param   {String} implicitChar implicit character
+         * @returns {Object} hintManager object
+         */
+        DocrHint.prototype.getHints = function () {
+            var hints = [];
+            this.match = this.editor.document.getRange(this.pos, this.editor.getCursorPos());
+
+            this.hintText = '';
+            this.removeSelection = true;
+            this.boolSetSelection = false;
+            this.deleteFirstNChars = 0;
+
+			var language 	= this.editor.getLanguageForSelection().getId();
+            var definitions = this.docDefinitions;
+			
+			if (definitions[language] === undefined) {
+				definitions = definitions.default;
+			} else {
+				definitions = definitions[language];
+			}
+			
+            switch (this.implicitChar) {
+            case "[[Type]]":
+				var defKeys = Object.keys(definitions.types);
+				for (var i = 0; i < defKeys.length; i++) {
+					hints.push(definitions.types[defKeys[i]]);
+				}
+				// Check for callbacks in the current file
+				var currentDoc = DocumentManager.getCurrentDocument().getText();
+				var callback = null;
+				while((callback = CALLBACK.exec(currentDoc)) != null) {
+					if (callback[1] != "[[callLink]]") {
+						hints.push(callback[1]);
+					}
+				}
+                break;
+			case "[[callLink]]":
+				var editor 	 	= EditorManager.getCurrentFullEditor();
+				var code 	 	= editor.document.getRange(this.pos,{ch:0,line:editor.lineCount()});
+				var funcNameRow = FUNC_DEFINITION_ROW.exec(code);
+				if (funcNameRow) {
+					var funcName = getFuncName(funcNameRow[1]);
+					if (funcName) {
+						hints.push(funcName);
+					}
+				}
+				// no break get all link possibilities as well!
+            case "[[Link]]":
+                var functionList = this.createFunctionList();
+                var functionSignature = this.getFunctionCodeTypes(this.editor, {
+                    line: this.insideDocPos.end,
+                    ch: 0
+                }, []);
+                var bestFuncs = [];
+                var otherFuncs = [];
+                for (var i = 0; i < functionList.length; i++) {
+                    var regexFuncTest = new RegExp('\[^a-zA-Z0-9\]' + functionList[i].name + '\\(');
+                    // remove the function name
+                    functionSignature.code = functionSignature.code.substr(functionSignature.code.indexOf('{'));
+                    if (regexFuncTest.test(functionSignature.code)) {
+                        bestFuncs.push(functionList[i].name);
+                    } else {
+                        otherFuncs.push(functionList[i].name);
+                    }
+                }
+                hints.push.apply(hints, bestFuncs.concat(otherFuncs));
+                break;
+			case "[[Name]]":
+			case "[[Author]]":
+				var _prefs = PreferencesManager.getExtensionPrefs('funcdocr');
+				var prefsAtName = _prefs.get('atName');
+				if (prefsAtName != '') {
+					hints.push(prefsAtName);
+				}
+				break;
+			case "[[Date]]":
+				var today = new Date();
+				var dd = today.getDate();
+					dd = dd < 10 ? '0'+dd : dd;
+				var mm = today.getMonth()+1; //January is 0!
+					mm = mm < 10 ? '0'+mm : mm;
+				var yyyy = today.getFullYear();
+				var hh = today.getHours();	
+				var mi = today.getMinutes();	
+					
+				hints.push(mm+'/'+dd+'/'+yyyy);	
+				hints.push(dd+'.'+mm+'.'+yyyy);	
+				hints.push(yyyy+'-'+mm+'-'+dd);	
+					
+				hints.push(mm+'/'+dd+'/'+yyyy+' '+hh+':'+mi);	
+				hints.push(dd+'.'+mm+'.'+yyyy+' '+hh+':'+mi);	
+				hints.push(yyyy+'-'+mm+'-'+dd+' '+hh+':'+mi);
+				break;
+			case "[[License]]":
+				hints.push('Creative Commons v3.0');	
+				hints.push('MIT license (MIT)');	
+				hints.push('W3C License (W3C)');	
+				break;
+            case "@":
+                var line = this.editor.document.getRange({
+                    line: this.pos.line,
+                    ch: 0
+                }, this.pos);
+
+                var match = /{\s*@$/.exec(line);
+
+                if (match) {
+                    this.deleteFirstNChars = 2;
+                } else {
+                    this.deleteFirstNChars = 1;
+                }
+
+              
+
+                for (var tag in definitions.tags) {
+                    hints.push(definitions.tags[tag]);
+                }
+
+                this.boolSetSelection = true;
+                break;
+            default:
+                // Here we have static suggestions included in the tag (i.e. [[Access<protected|public|private>]])
+                var hintsStartIndex = this.implicitChar.indexOf('<');
+                var hintsStopIndex = this.implicitChar.indexOf('>');
+                    
+                if (hintsStartIndex !== -1 && hintsStopIndex !== -1 && hintsStopIndex > hintsStartIndex) {
+                    var hintsRaw = this.implicitChar.substring(hintsStartIndex+1, hintsStopIndex);
+                    hints = hintsRaw.split('|');
+                }
+                break;
+            }
+
+
+            this.removeSelection = (this.removeSelection && this.match !== '') ? false : this.removeSelection;
+
+            hints = this.removeWrongHints(hints);
+
+            return {
+                hints: hints,
+                match: this.match,
+                selectInitial: true,
+                handleWideResults: false
+            };
+        };
+
+		/**
+		 * Return the function name for a special row
+		 * Array.prototype.abc = function() { => abc
+		 * function cool() { => cool
+		 * @param   {String} row row where the function is declared
+		 * @returns {String} the function name
+		 */
+		function getFuncName(row) {
+			row = row.trim();
+			// multiline,caseinsensitive
+			var regex = new RegExp(FUNC_DEFINITION.source, 'mi');
+
+			var matches 		= null;
+			var multicomment 	= null;
+			var match_func 		= false;
+			matches = regex.exec(row);
+			
+			if (matches) {
+				// matches[0] = all
+				// matches[2] = '''function_name''' or matches[4] if matches[2] undefined or matches[5] if both undefined
+				// get the function name
+				// start_pos
+				for (var i = 0; i < matches.length; i++) {
+					if (matches[i]) {
+						matches[i] = matches[i].trim();
+					}
+				}
+				if (matches[2]) {
+					match_func = matches[2];
+				} else if (matches[4]) {
+					match_func = matches[4];
+				} else if (matches[5]) {
+					match_func = matches[5];
+				}  else if (matches[7]) {
+					// prototype or static
+					if (matches[8] == "prototype.") {
+						match_func = matches[9];
+					} else if (!matches[8]) {
+						match_func = matches[9];
+					}
+				} else if(matches[11]) { // ECMAscript 6 function
+					match_func = matches[11];					
+				}
+				if (match_func) {
+					var end_func_name = match_func.search(/( |\(|$)/);
+					if (end_func_name >= 0) {
+						match_func = match_func.substring(0,end_func_name).trim();
+					}
+				}
+			}
+			
+			return match_func;
+		}
+
+        /**
+         * Remove hints that doesn't match the current this.match
+         * @param   {Array} hints hints that are availabe
+         * @returns {Array} hints that matches this.match
+         */
+        DocrHint.prototype.removeWrongHints = function (hints) {
+            var result = [];
+            for (var i = 0; i < hints.length; i++) {
+                if (hints[i].indexOf(this.match) >= 0) {
+                    result.push(hints[i]);
+                }
+            }
+            return result;
+        };
+
+        /**
+         * Inserts the hint
+         */
+        DocrHint.prototype.insertHint = function (hint) {
+            // Document objects represent file contents
+            var currentDoc = this.editor.document;
+
+            // Where the range end that should be replaced
+            var start = {
+                line: this.pos.line,
+                ch: this.pos.ch - this.deleteFirstNChars
+            };
+            var end = {
+                line: this.pos.line,
+                ch: this.pos.ch + ((this.removeSelection) ? this.selection.length : this.match.length)
+            };
+
+
+            // Add some text in our document
+            currentDoc.replaceRange(hint, start, end);
+
+            var hintContainsField = DOCBLOCK_FIELD.exec(hint);
+            if (this.boolSetSelection && hintContainsField) {
+                var match = /\[\[[a-z]+\]\]/i.exec(hint);
+                if (match) {
+                    var selectionEnd = this.pos.ch - this.deleteFirstNChars + match.index;
+                    var selectionStart = selectionEnd + match[0].length;
+                    this.setSelection(this.editor, {
+                        line: this.pos.line,
+                        ch: selectionStart
+                    }, {
+                        line: this.pos.line,
+                        ch: selectionEnd
+                    });
+                }
+            }
+
+        };
+
+        return DocrHint;
+    });
